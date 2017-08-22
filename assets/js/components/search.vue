123edd0053ec621708ce513fff2fe31c4aed1b88
Josh LaMar <jolamar@iu.edu>
2017-08-22 13:35:43 -0600

    make search more vue-like

--- assets/js/components/search.vue
+++ assets/js/components/search.vue
@@ -1,12 +1,16 @@
 <template>
-    <form action="search" class="m-bottom-lg">
+    <form @submit.prevent="search" class="m-bottom-lg">
         <label for="search-input">Search docs</label>
-        <input type="text" id="search-input">
+        <input v-model="searchInput" type="text" id="search-input">
     </form>
 </template>
 
 <script>
     module.exports = {
-        
+        data: function() {
+            return {
+                searchInput: ""
+            }
+        }
     }
 </script>
