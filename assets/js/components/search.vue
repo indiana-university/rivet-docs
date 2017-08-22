c10408a37a725404172acf4ade54b7c674906b87
Austin Lord <ahlord@iu.edu>
2017-08-22 16:41:30 -0400

    add lunr index in search component

--- assets/js/components/search.vue
+++ assets/js/components/search.vue
@@ -1,22 +1,47 @@
 <template>
-    <form @submit.prevent="search" class="m-bottom-lg">
-        <label for="search-input">Search docs</label>
-        <input v-model="searchInput" type="text" id="search-input">
-    </form>
+<form @submit.prevent="search" class="m-bottom-lg">
+    <label for="search-input">Search docs</label>
+    <input v-model="query" type="text" id="search-input">
+    <button type="submit" class="u-v-hide">Search</button>
+</form>
 </template>
 
 <script>
-    module.exports = {
-        data: function() {
-            return {
-                searchInput: ""
-            }
-        },
-        methods: {
-            search: function () {
-                //https://www.npmjs.com/package/axios
-                console.log("probably use axios for ajax")
+const http = require('axios');
+const lunr = require('lunr');
+
+module.exports = {
+    data: function() {
+        return {
+            query: "",
+            index: null
+        }
+    },
+    methods: {
+        search: function() {
+            if(this.index) {
+                var result = this.index.search(this.query)
+                console.log(result);
             }
         }
+    },
+    mounted() {
+        http.get('/lunr.json')
+            .then((response) => {
+                this.index = lunr(function() {
+                    this.ref('uri')
+                    this.field('content')
+                    this.field('tags')
+                    this.field('title')
+
+                    response.data.forEach(function(doc) {
+                        this.add(doc)
+                    }, this)
+                })
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
     }
+}
 </script>
--- assets/js/vue-main.js
+++ assets/js/vue-main.js
@@ -1,8 +1,9 @@
 /*
     swap vue.min out for vue if you need vue devtools
 */
-// const Vue = require('vue/dist/vue');
-const Vue = require('vue/dist/vue.min');
+const Vue = require('vue/dist/vue');
+// const Vue = require('vue/dist/vue.min');
+
 
 new Vue({
     el: '#wrapper',
