637c58a30c03cf1ad207b896a037ca7d1853c9ef
Austin Lord <ahlord@iu.edu>
2017-08-24 13:49:11 -0400

    add description to index

--- search-indexer.js
+++ search-indexer.js
@@ -8,90 +8,106 @@ var path = require('path');
 module.exports = indexer;
 
 
-function indexer(input, output){
-	var self = this;
-	var stream;
-	this.list = [];
+function indexer(input, output) {
+    var self = this;
+    var stream;
+    this.list = [];
 
-	//defaults
-	this.input = 'content/**';
-	this.output = 'static/site-index.json';
+    //defaults
+    this.input = 'content/**';
+    this.output = 'static/site-index.json';
 
-	this.baseDir = path.dirname(this.input);
+    this.baseDir = path.dirname(this.input);
 }
 
 indexer.prototype.setInput = function(input) {
-	this.input = input;
+    this.input = input;
 }
 
 indexer.prototype.setOutput = function(output) {
-	this.output = output;
+    this.output = output;
 }
 
-indexer.prototype.index = function(input, output){
-	var self = this;
+indexer.prototype.index = function(input, output) {
+    var self = this;
 
-	if (input){
-		self.input = input;
-	}
+    if (input) {
+        self.input = input;
+    }
 
-	if (output){
-		self.output = output;
-	}
+    if (output) {
+        self.output = output;
+    }
 
-	self.list = [];
-	self.stream = fs.createWriteStream(self.output);
-	self.readDirectory(self.input);
-	self.stream.write(JSON.stringify(self.list, null,4) );
-	self.stream.end();
+    self.list = [];
+    self.stream = fs.createWriteStream(self.output);
+    self.readDirectory(self.input);
+    self.stream.write(JSON.stringify(self.list, null, 4));
+    self.stream.end();
 }
 
 
-indexer.prototype.readDirectory = function(path){
-	var self = this;
-	var files = glob.sync(path);
-	var len = files.length;
-	for (var i=0;i<len;i++){
-		var stats = fs.lstatSync(files[i]);
-		if (!stats.isDirectory()){
-			self.readFile(files[i]);
-		}
-	}
-  	return true;
+indexer.prototype.readDirectory = function(path) {
+    var self = this;
+    var files = glob.sync(path);
+    var len = files.length;
+    for (var i = 0; i < len; i++) {
+        var stats = fs.lstatSync(files[i]);
+        if (!stats.isDirectory()) {
+            self.readFile(files[i]);
+        }
+    }
+    return true;
 }
 
-indexer.prototype.readFile = function(filePath){
-	var self = this;
-	var ext = path.extname(filePath);
-	var meta = matter.read(filePath, {delims: '+++', lang:'toml'});
-	if (meta.data.draft === true){
-		return;
-	}
+indexer.prototype.readFile = function(filePath) {
+    var self = this;
+    var ext = path.extname(filePath);
+    var meta = matter.read(filePath, {
+        delims: '+++',
+        lang: 'toml'
+    });
+    if (meta.data.draft === true) {
+        return;
+    }
 
-	if (ext == '.md'){
-		var plainText = removeMd(meta.content);
-	} else {
-		var plainText = striptags(meta.content);
-	}
+    if (ext == '.md') {
+        var plainText = removeMd(meta.content);
+    } else {
+        var plainText = striptags(meta.content);
+    }
 
-	var uri = '/' + filePath.substring(0,filePath.lastIndexOf('.'));
-	uri = uri.replace(self.baseDir +'/', '');
+    var uri = '/' + filePath.substring(0, filePath.lastIndexOf('.'));
+    uri = uri.replace(self.baseDir + '/', '');
     uri = uri.replace(/\/_index$/, '')
 
-	if (meta.data.slug !=  undefined){
-		uri = path.dirname(uri) + meta.data.slug;
-	}
+    if (meta.data.slug != undefined) {
+        uri = path.dirname(uri) + meta.data.slug;
+    }
 
-	if (meta.data.url != undefined){
-		uri = meta.data.url
-	}
+    if (meta.data.url != undefined) {
+        uri = meta.data.url
+    }
 
-	var tags = [];
+    let description = meta.content.substring(0, 144);
 
-	if (meta.data.tags != undefined){
-		tags = meta.data.tags;
-	}
+    if (meta.data.description != undefined) {
+        description = meta.data.description
+    }
 
-	var item = {'uri' : uri , 'title' : meta.data.title, 'content':plainText, 'tags':tags, 'rawContent':meta.content};
-	self.list.push(item);
+    var tags = [];
+
+    if (meta.data.tags != undefined) {
+        tags = meta.data.tags;
+    }
+
+    var item = {
+        'uri': uri,
+        'title': meta.data.title,
+        'content': plainText,
+        'rawContent': meta.content,
+        'tags': tags,
+        'description': description
+    };
+    self.list.push(item);
 }
