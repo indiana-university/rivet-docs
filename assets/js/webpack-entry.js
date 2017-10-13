3f22a00b92e31f8d5c52d29a246f032fe3b4decf
Austin Lord <ahlord@iu.edu>
2017-10-13 14:25:03 -0400

    rewrite auto-drawer-biggerator
    - previous code incorrectly watched navIsVisible variable, which is not connect to the drawer
    - add isDrawerOpen variable
    - add an additional listener to the drawer button
    - calculate the drawer height when it opens

--- assets/js/webpack-entry.js
+++ assets/js/webpack-entry.js
@@ -13,26 +13,33 @@ new Vue({
     },
     data: {
         // Controls the toggle of the section nav menu on mobile
-        navIsVisible: false
+        navIsVisible: false,
+        isDrawerOpen: false,
     },
     methods: {
         // Toggles the visibility of the section nav on mobile
         toggleNav: function() {
             this.navIsVisible = !this.navIsVisible;
+        },
+
+        // Listener for drawer toggle in header
+        // Note: drawer show/hide is handled in Rivet js
+        // this is an additional listener to set the height of the drawer when it is opened
+        toggleDrawer() {
+            this.isDrawerOpen = !this.isDrawerOpen
+
+            this.$nextTick(()=>{
+                if(this.isDrawerOpen) {
+                    let drawer = document.querySelector('.rvt-drawer')
+                    // set the drawer height to the height of the page minus 70px for the header
+                    drawer.style.height = (document.body.scrollHeight-70)+"px"
+                }
+            })
         }
     },
     mounted() {
 
     },
-    watch: {
-        navIsVisible(isVisible) {
-            if(isVisible) {
-                drawer = document.querySelector('.rvt-drawer')
-                // set the drawer height to the height of the page minus 70px for the header
-                drawer.style.height = (document.body.scrollHeight-70)+"px"
-            }
-        }
-    }
 })
 
 const Clipboard = require('clipboard')
