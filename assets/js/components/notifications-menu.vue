53c1051d83283df24e76835ee71d84011582a065
Austin Lord <ahlord@iu.edu>
2018-02-15 16:58:15 -0500

    wire up notification center

--- assets/js/components/notifications-menu.vue
+++ assets/js/components/notifications-menu.vue
@@ -70,7 +70,7 @@
 
             <div class="rvt-notifications__actions">
                 <a v-if="!errorLoadingNotifications" class="rvt-button rvt-button--secondary rvt-display-block rvt-text-center rvt-m-bottom-xs" :href="baseURL + 'notification-center'">More notifications</a>
-                <a class="rvt-button rvt-notifications__actions-mail" href="#0">
+                <a class="rvt-button rvt-notifications__actions-mail" href="https://list.iu.edu/sympa/subscribe/rivet-l">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
                         <title>Envelope icon</title>
                         <path fill="currentColor" d="M13.5,3H2.5A1.5,1.5,0,0,0,1,4.5v8A1.5,1.5,0,0,0,2.5,14h11A1.5,1.5,0,0,0,15,12.5v-8A1.5,1.5,0,0,0,13.5,3ZM11.41,5,8,7.77,4.59,5ZM3,12V6.29L7.11,9.62l.12.08a1.5,1.5,0,0,0,1.54,0L13,6.29V12Z"/>
