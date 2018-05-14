fc6f581c2cc948545dca0510a6b8843ed926a5f2
Josh LaMar <jolamar@iu.edu>
2018-05-14 09:08:15 -0600

    vue minified

--- assets/js/webpack-entry.js
+++ assets/js/webpack-entry.js
@@ -1,8 +1,8 @@
 /*
     swap vue.min out for vue if you need vue devtools
 */
-const Vue = require('vue/dist/vue');
-// const Vue = require('vue/dist/vue.min')
+// const Vue = require('vue/dist/vue');
+const Vue = require('vue/dist/vue.min')
 const VeeValidate = require('vee-validate')
 Vue.use(VeeValidate);
 
