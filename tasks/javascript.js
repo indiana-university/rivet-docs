d910ab4344f6d5d1863dfb89e10fea613cba0968
Rachel <racmocon@gmail.com>
2019-05-07 09:39:31 -0400

    Cleaned up files and removed unnecessary packages

--- tasks/javascript.js
+++ tasks/javascript.js
@@ -8,48 +8,9 @@ const babel = require('rollup-plugin-babel');
 const babelGulp = require("gulp-babel");
 const commonJS = require('rollup-plugin-commonjs');
 const concat = require("gulp-concat");
-const gutil = require("gulp-util");
 const resolve = require('rollup-plugin-node-resolve');
 const rollup = require('rollup');
 const uglify = require("gulp-uglify");
-
-
-/**
- * gulp.task('transpileJS', () => {
-  return rollup.rollup({
-    input: 'assets/js/rollup-entry.js',
-    plugins: [resolve(), commonJS(), babel({ runtimeHelpers: true })]
-  }).then(bundle => {
-    return bundle.write({
-      file: 'tmp/rollup-entry.js',
-      format: 'umd',
-      name: 'RivetDocs'
-    });
-  });
-});
- * 
- */
-
-/**
-   * gulp.task("js", ["transpileJS"], function() {
-    gulp
-      .src(["tmp/rollup-entry.js", "node_modules/rivet-uits/js/rivet.js"])
-      .pipe(concat("rivet-docs.js"))
-      .pipe(uglify())
-      .pipe(gulp.dest("./static/js"));
-  
-    gulp
-      .src(["assets/js/telemetrics.js"])
-      .pipe(babelGulp())
-      .pipe(concat("telemetrics.js"))
-      .pipe(uglify())
-      .pipe(gulp.dest("./static/js"));
-      
-    gulp.src('assets/js/highlight.pack.js')
-      .pipe(gulp.dest('./static/js'));
-  });
-   * 
-   */
 
 module.exports = {
 
